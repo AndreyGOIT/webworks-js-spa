018c70864278817822a75f5acb598c93dae4dc86
AndreyGOIT <ae.borga@gmail.com>
2025-02-14 10:12:58 +0200

    add slogan img and text to main

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const pages = {
-    home: `<div class='container hero-img'>
+    home: `<div class='hero-img'>
     <h1>WebWorks Studio</h1>
-    </div>`,
+    </div>
+    <div class='content-block'>
+        <img src='/images/slogan_kuva.png' alt='Company slogan image' class='slogan-img'>
+        <div class='content-text'>
+            <h2>ABOUT WebWorks Studio</h2>
+            <p>WebWorks Studio on nykyaikainen ohjelmistokehityksen yritys, joka keskittyy täyden palvelun verkkosivustojen ja sovellusten suunnitteluun ja toteutukseen. Tarjoamme räätälöityjä ratkaisuja, jotka vastaavat asiakkaidemme tarpeita ja auttavat heidän liiketoimintaansa kasvamaan digitaalisessa ympäristössä.
+            <br/>
+            Olemme ylpeitä siitä, että yhdistämme luovuuden, teknisen asiantuntemuksen ja nykyaikaiset teknologiat tuottaaksemme korkealaatuisia ja käyttäjäystävällisiä verkkopalveluita. Olipa kyseessä yrityksen kotisivu, verkkokauppa tai räätälöity sovellus, tiimimme huolehtii koko kehitysprosessista alusta loppuun.
+            <br/>
+            Laita ideasi käytäntöön WebWorks Studion kanssa!</p>
+        </div>
+    </div>
+    <div><h2>“Teemme digitaalisen helpoksi”</h2></div>`,
     tuotteet: "<h2>Tuotteet</h2><p>Tässä on tuotteemme.</p>",
     yhteystiedot: "<h2>Yhteystiedot</h2><p>Ota yhteyttä meihin!</p>",
     henkilokunta: "<h2>Henkilökunta</h2><div id='staff-table'></div>"
